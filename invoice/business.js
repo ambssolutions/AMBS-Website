4d9c038e4770257784079b22d2325c9c5d7aefb5
ambssolutions <ambssolutionslimited@gmail.com>
2026-09-20 20:24:05 +1200

    Add files via upload

---
+++ invoice/business.js
@@ -0,0 +1,26 @@
+// AMBS business details used on online invoices.
+// Bank details live here (not in the link), so a link can't show a different account.
+// Edit these values if your details change, then commit the file to GitHub.
+window.AMBS_BUSINESS = {
+  "businessName": "AMBS Solutions Limited",
+  "tagline": "Your Complete Digital Partner",
+  "address": "",
+  "phone": "+64 22 099 9578",
+  "email": "hello@ambs.co.nz",
+  "website": "www.ambs.co.nz",
+  "gstNumber": "149-630-775",
+  "nzbn": "94-29053939401",
+  "companyNumber": "",
+  "bank": "ANZ",
+  "bankName": "AMBS SOLUTIONS LIMITED",
+  "bankAccount": "06-0851-0555530-00",
+  "swift": "",
+  "brandColor": "#0f0f0f",
+  "terms": "Thank you for your business",
+  "showThankYou": true,
+  "thankYouText": "Thank you for your business!",
+  "thankYouSub": "We appreciate working with you.",
+  "thankYouColor": "#111111",
+  "showAdvice": false,
+  "paymentTermsDays": 7
+};
